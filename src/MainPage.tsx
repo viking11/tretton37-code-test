6cd55ff88e0af987579d3a3659dc2740fc0940b2
Kasper Andersen <kasper.andersen@softhouse.se>
2023-11-03 09:33:11 +0100

    finish initial setup

---
+++ src/MainPage.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MainWrapper = styled.div``;
+
+const MainPage = () => {
+  return (
+    <MainWrapper>
+      <p>The fellowship of the tretton37</p>
+    </MainWrapper>
+  );
+};
+
+export default MainPage;
