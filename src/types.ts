7ee9f9cfb2392ff57e10eecdf1fc2d9f578b9607
Kasper Andersen <kasper.andersen@softhouse.se>
2023-11-03 12:50:43 +0100

    create basic layout

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export type CollegueType = {
+  area: string;
+  email: string;
+  gitHub: string;
+  highlighted: boolean;
+  imagePortraitUrl: string;
+  imageWallOfLeetUrl: string;
+  linkedIn: string;
+  mainText: string;
+  manager: string;
+  name: string;
+  office: string;
+  orgUnit: string;
+  phoneNumber: string;
+  primaryRole: string;
+  published: true;
+  secondaryRole: string;
+  stackOverflow: string;
+  twitter: string;
+};
